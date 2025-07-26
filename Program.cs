1067c299a25bcb4a0e3e567d7372c5ab26dac820
harinivas-28 <harinivasg28704@gmail.com>
2025-07-26 17:28:24 +0530

    Moved practise pace from last commit to the arrays (#2)

--- Program.cs
+++ Program.cs
@@ -30,6 +30,73 @@ namespace HelloWorld // namespace is a container for classes and other namespace
             // user input
             string name = Console.ReadLine();
             Console.WriteLine(name);
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(age);
+            // operators are similar to java
+            // C# Math
+            Console.WriteLine(Math.Max(5, 10));
+            Console.WriteLine(Math.Min(5, 10));
+            Console.WriteLine("Sqrt of 64 is: " + Math.Sqrt(64));
+            Console.WriteLine(Math.Abs(-4.7));
+            Console.WriteLine(Math.Round(9.99));
+            // Strings
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine(txt.Length);
+            Console.WriteLine(txt.ToUpper());
+            Console.WriteLine(txt.ToLower());
+            Console.WriteLine(string.Concat(txt, name));
+            // String interpolation
+            string printName = $"My name is: {txt} {name}";
+            Console.WriteLine(printName);
+            Console.WriteLine("First Character in name: " + name[0]);
+            Console.WriteLine(name.IndexOf("i"));
+            string name2 = "John Doe";
+            int charPos = name2.IndexOf("D");
+            string lastName = name2.Substring(charPos);
+            Console.WriteLine(lastName);
+            // special characters are same as in java
+            // Booleans
+            bool isCSharpFun = true;
+            Console.WriteLine(isCSharpFun);
+            if (age > 18 && age < 100) 
+            {
+                Console.WriteLine("Can Vote!");
+            } else if (age>100){
+                Console.WriteLine("Die Bro!");
+            } else {
+                Console.WriteLine("Cannot Vote!");
+            }
+            // ternary is similar in java
+            string result = (age > 18) ? "Can Vote" : "Cannot Vote";
+            int day = 3;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                default:
+                    Console.WriteLine("Other days");
+                    break;
+            }
+            // while and for loops are same as in the java;
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach(string i in cars){
+                Console.WriteLine(i);
+            }
+            // loop control statements like break and continue are similar to java
+            // ARRAYS
+            string[] names;
+            //names = {"Volvo", "BMW","Ford", "Mazda"}; This is Wrong
+            names = new string[] { "Volvo", "BMW", "Ford", "Mazda" };
+            int[] nums = { 1, 2, 4 };
+            Console.WriteLine(string.Join(", ", nums)); // 1, 2, 3
 
         }
     }
